60bf65abe7e12e5c844defe36f0f6d4365fb4ab8
Santosh Patel <santoshpatelvns5@gmail.com>
2026-02-03 11:16:54 +0530

    WriteFileSync create ds-b.txt file

--- fsmodule/server.js
+++ fsmodule/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import fs from "fs";
 const app=express();
 
 const PORT=3000;
@@ -7,6 +7,8 @@ const PORT=3000;
 app.get("/",(req,res)=>{
     res.send("Hello from Express Server");
 })
+// WriteFile Sync
+fs.writeFileSync("./ds-b.tsxt","Hello ABES College Students");
 
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT);
