96f50f9b2a31e104fb567ced56fbdc81f3e91f94
Santosh Patel <santoshpatelvns5@gmail.com>
2026-02-03 11:56:29 +0530

    classwork

--- fsmodule/server.js
+++ fsmodule/server.js
@@ -33,6 +33,60 @@ const d1=fs.readFile("./demo.txt","utf-8",(err,data)=>{
         console.log(data);
     }
 })
+// Classwork A1,A2,A3,A4,A5,A6 || B1.B2,B3,B4,B5,B6
+
+// 3 Sync methods and 3 Async methods of fs module
+fs.writeFileSync("./A1.txt","this is A1 file");
+fs.writeFileSync("./A2.txt","this is A2 file");
+fs.writeFileSync("./A3.txt","this is A3 file");
+
+// Async methods
+
+fs.writeFile("./B1.txt","this is the B1 file",()=>{
+    console.log("B1 file created");
+})
+fs.writeFile("./B2.txt","this is the B2 file",()=>{
+    console.log("B2 file created");
+}
+)
+fs.writeFile("./B3.txt","this is the B3 file",()=>{
+    console.log("B3 file created");
+}
+)
+
+// readFileSync || writeFileSync
+const A2data=fs.readFileSync("./A2.txt","utf-8");
+console.log(A2data);    
+const A3data=fs.readFileSync("./A3.txt","utf-8");
+console.log(A3data);
+const A1data=fs.readFileSync("./A1.txt","utf-8");
+console.log(A1data);
+// readFile Async
+fs.readFile("./B1.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+}
+)
+fs.readFile("./B2.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
+fs.readFile("./B3.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
 
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT);
