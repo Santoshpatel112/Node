8208779a483dc7565ff1a944ab37fef292cb51d4
Santosh Patel <santoshpatelvns5@gmail.com>
2026-02-03 11:43:57 +0530

    readFile async and writeFile Async

--- fsmodule/server.js
+++ fsmodule/server.js
@@ -8,15 +8,32 @@ app.get("/",(req,res)=>{
     res.send("Hello from Express Server");
 })
 // WriteFile Sync
-fs.writeFileSync("./ds-b.tsxt","Hello ABES College Students");
-
-
+fs.writeFileSync("./ds-b.txt","Hello ABES College Students");
 fs.writeFileSync("./Abes.txt","Hii ABES Students");
+
+// readFileSync
 const data=fs.readFileSync("./Abes.txt","utf-8");
 console.log(data);
-
-const data2=fs.readFileSync("./ds-b.tsxt","utf-8");
+const data2=fs.readFileSync("./ds-b.txt","utf-8");
 console.log(data2);
+
+
+// writeFile Async
+
+fs.writeFile("./demo.txt","this is the demo file of the write file async",()=>{
+    console.log("File is created successfully");
+})
+
+// readFile Async
+const d1=fs.readFile("./demo.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
+
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT);
 })
