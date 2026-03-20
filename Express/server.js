2bdcca2aeda7714abc77372a1242812da995cb28
Santosh Patel <santoshpatelvns5@gmail.com>
2026-03-20 15:03:33 +0530

    API for read data and read data by id

--- Express/server.js
+++ Express/server.js
@@ -18,6 +18,19 @@ const student =[
         }
 ]
 
+//API for read data
+
+app.get("/read",(req,res)=>{
+    res.json(student)
+})
+//api read data by id
+
+app.get("/read/:id",(req,res)=>{
+    const id=req.params.id;
+        res.json(student.find((s)=>s.id==id));
+    
+});
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 }); 
