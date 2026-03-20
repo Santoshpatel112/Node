cb6c895a92c02f5dc14c4d75219107106d0a820a
Santosh Patel <santoshpatelvns5@gmail.com>
2026-03-20 14:40:40 +0530

    database json

--- Express/server.js
+++ Express/server.js
@@ -6,6 +6,18 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
+// data base {json}
+
+const student =[
+    {
+        id:1,
+        name:"deepak"},
+        {
+            id:2,
+            name:"rupak"
+        }
+]
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 }); 
